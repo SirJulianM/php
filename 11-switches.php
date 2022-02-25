cfcfa9c663e272710963c3410238372f5969518b
SirJulianM <ing_julianmadrid@hotmail.com>
2022-02-25 15:16:16 -0500

    Switches en PHP

---
+++ 11-switches.php
@@ -0,0 +1,40 @@
+<?php
+
+$dia = 9;
+
+switch ($dia) 
+{
+    case 1:
+        echo "Hoy es domingo";
+        break;
+
+    case 2:
+        echo "Hoy es lunes";
+        break;
+    
+    case 3:
+        echo "Hoy es martes";
+        break;
+
+    case 4:
+        echo "Hoy es miercoles";
+        break;
+
+    case 5:
+        echo "Hoy es jueves";
+        break;
+
+    case 6:
+        echo "Hoy es viernes";
+        break;
+
+    case 7:
+        echo "Hoy es sábado";
+        break;
+    
+    default:
+        echo "No se que día es";
+        break;
+}
+
+?>
