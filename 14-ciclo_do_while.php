24c636bee6d77438aed5bf620e063c6097a4c9c9
SirJulianM <ing_julianmadrid@hotmail.com>
2022-02-25 17:53:36 -0500

    Ciclo do-while

---
+++ 14-ciclo_do_while.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 0;
+do 
+{
+    echo $a."<br />";
+    $a++;
+} 
+
+while ($a <= 10);
+
+/*
+
+Do while se diferencia del while en que el primero se ejecuta al menos una sola vez, ya que primero es la
+función y luego el ciclo, mientras que el while es primero el ciclo y luego la función
+
+*/
+
+?>
