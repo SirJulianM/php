b12958396dada0adf44181a4ed5e10befa95052d
SirJulianM <ing_julianmadrid@hotmail.com>
2022-02-25 18:02:01 -0500

    Arreglos en PHP

---
+++ 17-arrays.php
@@ -0,0 +1,24 @@
+<?php 
+
+/* 
+
+Hay varios tipos de arreglos 
+
+- numéricos
+- asociativos
+- multidimensionales
+
+Las cadenas también son arreglos
+
+*/
+
+$arrayNumerico = array(2, "José", 2.398, 7);
+echo $arrayNumerico[0]."<br />";
+$arrayNumerico[1] = "Juan";
+echo $arrayNumerico[1]."<br />";
+
+//Ejemplo cadena
+$arrayNombre = "Julián";
+echo $arrayNombre[1]."<br />";
+
+?>
