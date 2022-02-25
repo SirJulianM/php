c314b869c77e85071fe9380beb8c827ef89c724b
SirJulianM <ing_julianmadrid@hotmail.com>
2022-02-25 15:09:52 -0500

    Operadores de asignación en PHP

---
+++ 08-operadores_asignacion.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = 5;
+echo $a;
+echo "<br /><hr />";
+$a += 7;
+echo $a;
+echo "<br /><hr />";
+$a += 7 + $a - 9;
+echo $a;
+echo "<br /><hr />";
+$a -= 3;
+echo $a;
+echo "<br /><hr />";
+$a *=5;
+echo $a;
+echo "<br /><hr />";
+$a /=19;
+echo $a;
+echo "<br /><hr />";
+$a %=5;
+echo $a;
+echo "<br /><hr />";
+?>
